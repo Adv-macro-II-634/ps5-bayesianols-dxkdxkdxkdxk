3749f9863d278fe398e5e4edfd0242f5de7e5a4e
XiangkunDai <xiangkunbendai@gmail.com>
2019-12-07 00:27:49 -0500

    Update PS5.m

--- PS5.m
+++ PS5.m
@@ -18,3 +18,13 @@ Y = lwage;
 X_wI = [ones(N,1) X];
 
 
+ols = fitlm(X,Y,'VarNames',{'educ','exper','smsa','black','south','lwage'});
+df = ols.DFE;                                     
+beta_hat_ini = table2array(ols.Coefficients(:, 1));  
+se_beta_hat_ini = ols.Coefficients(:,2);              
+var_beta_hat_ini = diag(ols.CoefficientCovariance);   
+sigma_sq_hat_ini = ols.MSE;                       
+sigma_hat_ini = ols.RMSE;
+sigma_var_ini = 2/df*(sigma_sq_hat_ini);
+
+save(fullfile(tempdir,'OLS_results'));
